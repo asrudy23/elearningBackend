bdc103a8236c12357fcd58e730ce1cd8b2b19412
asrudy23 <ascensionrudy@gmail.com>
2025-07-14 08:18:07 +0100

    first commit

---
+++ src/main/java/com/elearningBackend/ElearningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elearningBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearningBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elearningBackend/ElearningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearningBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
